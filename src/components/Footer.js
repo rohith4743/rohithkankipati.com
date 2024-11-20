ece904fbfd841936e5ffc508c34a581435dc4a03
rohith4743 <rohithkankipati4743@gmail.com>
2024-11-20 08:50:57 -0500

    linkedIn change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="text-center p-4">
-      <p>Connect with me on <a href="https://github.com/rohith4743">GitHub</a> | <a href="https://www.linkedin.com/in/rohithkankipati/">LinkedIn</a></p>
+      <p>Connect with me on <a href="https://github.com/rohith4743">GitHub</a> | <a href="https://www.linkedin.com/in/rohith-kankipati-21b51519b/">LinkedIn</a></p>
       <p>&copy; 2023 Rohith Kankipati</p>
     </footer>
   );
